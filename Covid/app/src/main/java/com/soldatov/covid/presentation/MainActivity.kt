4dc3288f0fad623764cd117412f43e760bc6cdb1
Vladimir <feelini09@gmail.com>
2021-08-25 18:27:37 +0300

    add map fragment

--- Covid/app/src/main/java/com/soldatov/covid/presentation/MainActivity.kt
+++ Covid/app/src/main/java/com/soldatov/covid/presentation/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.soldatov.covid.R
 import com.soldatov.covid.data.api.ApiHelper
 import com.soldatov.covid.data.api.NetworkService
@@ -14,15 +18,17 @@ import com.soldatov.covid.domain.models.CovidInfo
 import com.soldatov.covid.utils.Status
 import com.soldatov.covid.utils.ViewModelFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var viewModel: MainActivityViewModel
     private lateinit var bottomNavigationView: BottomNavigationView
+    private val mapFragment = SupportMapFragment.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavigationView = findViewById(R.id.bottomNavView)
+        bottomNavigationView.selectedItemId = R.id.map
         setupViewModel()
         setupObservers()
     }
@@ -64,15 +70,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun addInfoFragment(covidInfo: CovidInfo){
+    private fun addInfoFragment(covidInfo: CovidInfo) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, InfoFragment(covidInfo))
             .commit()
     }
 
-    private fun addMapFragment(){
+    private fun addMapFragment() {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, MapFragment())
+            .replace(R.id.fragment_container, mapFragment)
             .commit()
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        p0.addMarker(MarkerOptions().position(LatLng(46.227600000000002, 2.2136999999999998)).title("Title"))
     }
 }
