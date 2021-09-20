725487f32f2bb4ac21dce33f7460d874bcebe768
Vladimir <feelini09@gmail.com>
2021-09-20 11:09:12 +0300

    ktor joke

--- server/src/Application.kt
+++ server/src/Application.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
@@ -17,5 +18,19 @@ fun Application.configureRouting(){
         get("/"){
             call.respondText("Hello World!")
         }
+
+        get("/joke"){
+            val firstName = call.parameters["firstName"] ?: return@get call.respondText("Bad Request")
+            val lastName = call.parameters["lastName"] ?: return@get call.respondText("Bad Request")
+            val joke = jokes.random()
+            val respond = "{ \"type\": \"success\", \"value\": {\"joke\": \"$firstName $lastName $joke\" } }"
+            call.respond(respond)
+        }
     }
 }
+
+val jokes = listOf(
+    "is the only man who has, literally, beaten the odds. With his fists.",
+    "keeps his friends close and his enemies closer. Close enough to drop them with one round house kick to the face.",
+    "can make fire using two ice cubes."
+)
