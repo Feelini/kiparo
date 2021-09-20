bd127ead9917bb1c40dae33adef5d91801c97d27
Vladimir <feelini09@gmail.com>
2021-09-20 10:39:16 +0300

    ktor example

--- server/src/Application.kt
+++ server/src/Application.kt
@@ -2,12 +2,20 @@ package com.example
 
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "localhost") {
+        configureRouting()
+    }.start(wait = true)
 }
 
+fun Application.configureRouting(){
+    routing {
+        get("/"){
+            call.respondText("Hello World!")
+        }
+    }
+}
